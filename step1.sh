dc3bbece1e6a5f941c4f71700364e45826549590
norcon <dennis@hecken.cc>
2023-12-29 13:05:55 +0100

    🛠️ step1.sh -> Added server URI, username, password, XML file path, and API path. Constructed XML request body for PROPFIND request. Executed PROPFIND request using curl and saved the response to a local XML file. Printed a message indicating the XML response has been saved.

--- step1.sh
+++ step1.sh
@@ -1,13 +1,22 @@
+```shellscript
 #!/bin/bash
 
+# Set the server URI
 SERVER_URI="https://nc.domain.tld"
+
+# Set the username for authentication
 USERNAME="your_username"
+
+# Set the password for authentication
 PASSWORD="your_password"
+
+# Set the local path for the XML file
 XML_FILE_PATH="./result.xml"
+
+# Set the API path for accessing the trashbin
 API_PATH="remote.php/dav/trashbin/<your_username>/trash"
 
-
-# PROPFIND request to get information about deleted files
+# Construct the XML request body for the PROPFIND request
 REQUEST_BODY=$(cat <<EOF
 <?xml version="1.0"?>
 <d:propfind xmlns:d="DAV:" xmlns:oc="http://nextcloud.org/ns">
@@ -22,7 +31,8 @@ REQUEST_BODY=$(cat <<EOF
 EOF
 )
 
-# Execute PROPFIND request and save the response to a local XML file
+# Execute the PROPFIND request using curl
+# Save the response to the local XML file
 curl "$SERVER_URI/$API_PATH/" \
   -H 'Content-Type: application/xml; charset=UTF-8' \
   -H 'Depth: 1' \
@@ -31,4 +41,6 @@ curl "$SERVER_URI/$API_PATH/" \
   --user "$USERNAME:$PASSWORD" \
   > "$XML_FILE_PATH"
 
+# Print a message indicating the XML response has been saved
 echo "XML response saved to: $XML_FILE_PATH"
+```
